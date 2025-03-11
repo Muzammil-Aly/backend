c9ad884b47cfdf9d520b076751dc600da7b4bbe4
Muzammil <muzammilnizaami9@gmail.com>
2025-03-11 22:02:17 +0500

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+// import express from "express";
+
+// const app = express();
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MOGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `MongoDB connected !! DB Host : ${connectionInstance.connection.host}`
+    );
+    // app.on("error", (error) => {
+    //   console.log("Error in connecting database", error);
+    //   throw error;
+    // });
+    // app.listen(process.env.PORT, () => {
+    //   console.log(`App is listening on port ${process.env.PORT}`);
+    // });
+  } catch (error) {
+    console.log("MOGODB connection failed", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
